a813fb0ca63c5c6794053b85f4efbc4b5766b182
adityasinghskit <adityasinghskit@gmail.com>
2022-03-31 09:53:53 +0530

    First commit

---
+++ scripts/menwatches.js
@@ -0,0 +1,356 @@
+let wdata=[
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hc9/h13/26908966813726/WFT38072AP01_NoColour.jpg_230Wx334H",
+		name:"Unisex 37.25 mm Reflex Vox Carbon Black Dial Silicone Smart Watch",
+		brand:"FASTRACK",
+		price:4995,
+		strprice:6995,
+		off:30,
+		strapc:"gray",
+		shape:"square",
+		occasion:"fitness",
+		category:"smart",
+		color:"multi",
+		order:"25",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h52/h33/14126318256158/205414866_9999.jpg_230Wx334H",
+		name:"Mens Black Dial Metallic Multi-Function Watch - GD- 1015- 33",
+		brand:"GIORDANO",
+		price:5453,
+		strprice:7790,
+		off:30,
+		strapc:"black",
+		shape:"circle",
+		occasion:"casual",
+		category:"analog",
+		color:"black",
+		order:"15",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h6a/heb/26909196222494/WTT90149AP01_NoColour.jpg_230Wx334H",
+		name:"Mens Black Dial Silicone Smart Watch - 90149AP01",
+		brand:"TITAN",
+		price:11995,
+		strprice:14995,
+		off:20,
+		strapc:"gray",
+		shape:"circle",
+		occasion:"casual",
+		category:"digital",
+		color:"multi",
+		order:"32",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h23/h32/26908974088222/WFT38072AP04_NoColour.jpg_230Wx334H",
+		name:"Unisex 37.25 mm Reflex Vox Flaming Red Dial Silicone Smart Watch",
+		brand:"FASTRACK",
+		price:4995,
+		strprice:6995,
+		off:30,
+		strapc:"red",
+		shape:"square",
+		occasion:"fitness",
+		category:"digital",
+		color:"multi",
+		order:"44",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hb6/hec/27002817314846/WTT1856QM01_NoColour.jpg_230Wx334H",
+		name:"Mens 39.5 mm Octane Aerobatics Grey Dial Stainless Steel Analogue Watch - 1856QM01",
+		brand:"TITAN",
+		price:10995,
+		strprice:"",
+		off:"",
+		strapc:"silver",
+		shape:"square",
+		occasion:"formal",
+		category:"analog",
+		color:"black",
+		order:"12",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hea/h1e/26856393932830/WFIAE-AX2428I_NoColour.jpg_230Wx334H",
+		name:"Mens 46 mm Black Dial Silicone Analogue Watch - AX2428I",
+		brand:"ARMANI",
+		price:5596,
+		strprice:6995,
+		off:20,
+		strapc:"black",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"black",
+		order:"18",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h16/h80/26782009098270/WTTNP1805NM02_NoColour.jpg_230Wx334H",
+		name:"Mens Gents NEO V Phase I Black Dial Stainless Steel Analogue Watch - 1805NM02",
+		brand:"TITAN",
+		price:6495,
+		strprice:"",
+		off:"",
+		strapc:"black",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"black",
+		order:"44",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hcf/h29/17432771657758/WNCTH1791778_NoColour.jpg_230Wx334H",
+		name:"Mens White Dial Silicone Multi-Function Watch",
+		brand:"TOMMY HILFIGER",
+		price:13750,
+		strprice:"",
+		off:"",
+		strapc:"blue",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"multi",
+		order:"36",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h39/h55/26943655804958/WTT1805QM01_NoColour.jpg_230Wx334H",
+		name:"Mens 49.2 x 42 mm NEO Splash Blue Dial Stainless Steel Analogue Watch",
+		brand:"TITAN",
+		price:6995,
+		strprice:"",
+		off:"",
+		strapc:"blue",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"multi",
+		order:"66",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h8e/h35/26943997181982/WTT1805QP01_NoColour.jpg_230Wx334H",
+		name:"Mens 49.2 x 42 mm NEO Splash Black Dial Fabric Analogue Watch",
+		brand:"TITAN",
+		price:5495,
+		strprice:"",
+		off:"",
+		strapc:"black",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"multi",
+		order:"88",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hc8/h28/26909194715166/WTT90149AP03_NoColour.jpg_230Wx334H",
+		name:"Mens Black Dial Silicone Smart Watch - 90149AP03",
+		brand:"TITAN",
+		price:11995,
+		strprice:14995,
+		off:20,
+		strapc:"blue",
+		shape:"circle",
+		occasion:"fitness",
+		category:"smart",
+		color:"multi",
+		order:"111",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h85/h4c/16072610873374/WTFC-F1-D_NoColour.jpg_230Wx334H",
+		name:"Unisex Black Dial Stainless Steel Smart Watch F1-D",
+		brand:"FRENCH CONNECTION",
+		price:7450,
+		strprice:"",
+		off:"",
+		strapc:"pink",
+		shape:"square",
+		occasion:"fitness",
+		category:"smart",
+		color:"multi",
+		order:"89",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hab/h9e/26844585263134/W-A2018BLK_NoColour.jpg_230Wx334H",
+		name:"Unisex 41 mm GTS 2 Mini Black Dial Silicone Amoled Smart Watch - A2018",
+		brand:"AMAZFIT",
+		price:6999,
+		strprice:"",
+		off:"",
+		strapc:"grey",
+		shape:"square",
+		occasion:"fitness",
+		category:"smart",
+		color:"multi",
+		order:"189",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hed/h22/26944439156766/WTT1805QM05_NoColour.jpg_230Wx334H",
+		name:"Mens 49.2 x 42 mm NEO Splash Black Dial Stainless Steel Analogue Watch",
+		brand:"TITAN",
+		price:5995,
+		strprice:"",
+		off:"",
+		strapc:"brown",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"multi",
+		order:"44",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/h3a/h58/27005592731678/WCAA1973_NoColour.jpg_230Wx334H",
+		name:"Mens 45-55 mm Black Dial Stainless Steel Analogue Watch - A1973",
+		brand:"CASIO",
+		price:4995,
+		strprice:"",
+		off:"",
+		strapc:"black",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"multi",
+		order:"66",
+	},
+	{
+		imgurl:"https://sslimages.shoppersstop.com/sys-master/images/hf9/hf9/26854104039454/WKCWGL2122303MN_NoColour.jpg_230Wx334H",
+		name:"Mens 43.5 mm Black Dial Stainless Steel Bracelet Analogue Watch - KCWGL2122303MN",
+		brand:"CASIO",
+		price:16995,
+		strprice:"",
+		off:"",
+		strapc:"silver",
+		shape:"circle",
+		occasion:"formal",
+		category:"analog",
+		color:"multi",
+		order:"22",
+	},
+]
+	showitems(wdata);
+	function showitems(arr){
+		document.querySelector("#items").innerHTML="";
+		arr.map(function(el){
+			let div1=document.createElement("div");
+			div1.setAttribute("class","item-card");
+			let img1=document.createElement("img");
+			img1.src=el.imgurl;
+			let brand1=document.createElement("p");
+			brand1.setAttribute("class","item-brand");
+			brand1.innerText=el.brand;
+			let name1=document.createElement("p");
+			name1.setAttribute("class","item-name");
+			name1.innerText=el.name;
+			let div2=document.createElement("div");
+			div2.setAttribute("class","item-pricediv");
+			let price1=document.createElement("p");
+			price1.setAttribute("class","item-price");
+			price1.innerText="₹"+el.price;
+			let strprice1=document.createElement("p");
+			strprice1.setAttribute("class","item-strprice");
+			if(el.strprice!=""){
+				strprice1.innerText="₹"+el.strprice;
+			}
+			strprice1.style.textDecoration="line-through";
+			let off1=document.createElement("p");
+			off1.setAttribute("class","item-off");
+			if(el.off!=""){
+				off1.innerText=el.off +"% off";
+			}
+			div2.append(price1,strprice1,off1);
+			add1=document.createElement("button");
+			add1.setAttribute("class","item-btn");
+			add1.innerText="Add to Cart";
+			add1.onclick=function() {
+				addtocart(el);
+			}
+			div1.append(img1,brand1,name1,div2,add1);
+			document.querySelector("#items").append(div1);
+		})
+	}
+
+	function addtocart(el) {
+		let cartarr=JSON.parse(localStorage.getItem("cartkey")) || [];
+		let dup=false;
+		cartarr.map(function(elm){
+			if(el.name==elm.name) {
+				dup=true;
+			}
+		})
+		if(!dup){
+		cartarr.push(el);
+		localStorage.setItem("cartkey",JSON.stringify(cartarr));
+		}
+		
+	}
+
+	function sortfunc0(){
+		let val=document.querySelector("#pop").value;
+		let narr=[];
+		if(val=="plh"){
+			narr=wdata.sort(function(a,b){
+				return a.price-b.price;
+			})
+		}else if(val=="phl"){
+			narr=wdata.sort(function(a,b){
+				return b.price-a.price;
+			})
+		}else if(val=="phl"){
+			narr=wdata.sort(function(a,b){
+				return b.price-a.price;
+			})
+		}else if(val=="disc"){
+			narr=wdata.sort(function(a,b){
+				return b.off-a.off;
+			})
+		}else if(val=="bs"){
+			narr=wdata.sort(function(a,b){
+				return b.order-a.order;
+			})
+		}
+		showitems(narr);
+	}
+	function sortfunc1(){
+		let val=document.querySelector("#strapc").value;
+		let narr=wdata.filter(x => x.strapc==val);
+		showitems(narr);
+	}
+	function sortfunc2(){
+		let val=document.querySelector("#shape").value;
+		let narr=wdata.filter(x => x.shape==val);
+		showitems(narr);
+	}
+	function sortfunc3(){
+		let val=document.querySelector("#occasion").value;
+		let narr=wdata.filter(x => x.occasion==val);
+		showitems(narr);
+	}
+	function sortfunc4(){
+		let val=document.querySelector("#category").value;
+		let narr=wdata.filter(x => x.category==val);
+		showitems(narr);
+	}
+	function sortfunc5(){
+		let val=document.querySelector("#brand").value;
+		let narr=wdata.filter(x => x.brand==val);
+		showitems(narr);
+	}
+	function sortfunc6(){
+		let val=document.querySelector("#off").value;
+		let narr=wdata.filter(x => x.off==val);
+		showitems(narr);
+	}
+	function sortfunc7(){
+		let val=document.querySelector("#color").value;
+		let narr=wdata.filter(x => x.color==val);
+		showitems(narr);
+	}
+	function sortfunc8(){
+		let val=document.querySelector("#price").value;
+		let narr=[];
+		if(val=="2000"){
+		narr=wdata.filter(x => (x.price>1000 && x.price<=2000));
+		}else if(val=="5000"){
+			narr=wdata.filter(x => (x.price>2000 && x.price<=5000));
+		}else if(val=="10000"){
+			narr=wdata.filter(x => (x.price>5000 && x.price<=10000));
+		}
+		showitems(narr);
+	}
